1214f43a6e625a24c13eff11d1b7e20d2ef56a05
saymepravdy <sosnilo.ma@gmail.com>
2023-10-10 22:29:58 +0300

    allow cors

--- src/main/java/com/soa/second/soa_second/config/JerseyConfiguration.java
+++ src/main/java/com/soa/second/soa_second/config/JerseyConfiguration.java
@@ -7,5 +7,6 @@ import org.springframework.context.annotation.Configuration;
 public class JerseyConfiguration extends ResourceConfig {
     public JerseyConfiguration() {
         packages("com.soa.second.soa_second.controller");
+        register(CorsResponseFilter.class);
     }
 }
