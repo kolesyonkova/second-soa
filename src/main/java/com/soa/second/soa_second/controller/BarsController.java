1ad43cd0d204115b37f60ecc6118f1bbbbd7fa8b
saymepravdy <sosnilo.ma@gmail.com>
2023-10-11 23:25:44 +0300

    handle exception

--- src/main/java/com/soa/second/soa_second/controller/BarsController.java
+++ src/main/java/com/soa/second/soa_second/controller/BarsController.java
@@ -2,11 +2,13 @@ package com.soa.second.soa_second.controller;
 
 import com.soa.second.soa_second.dto.LabWorkDto;
 import com.soa.second.soa_second.service.BarsService;
+import feign.FeignException;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @RestController
 @Path("/bars")
@@ -17,7 +19,12 @@ public class BarsController {
 
     @PUT
     @Path("/labwork/{labwork-id}/difficulty/increase/{steps-count}")
-    public LabWorkDto increaseStepsCount(@PathParam("labwork-id") Integer id, @PathParam("steps-count") Integer stepsCount) {
-        return barsService.increaseStepsCount(id, stepsCount);
+    public Response increaseStepsCount(@PathParam("labwork-id") Integer id, @PathParam("steps-count") Integer stepsCount) {
+        try {
+            return Response.ok(barsService.increaseStepsCount(id, stepsCount)).build();
+        } catch (FeignException e) {
+            return Response.status(e.status() == -1 ? Response.Status.SERVICE_UNAVAILABLE.getStatusCode() : e.status())
+                    .entity(e.contentUTF8()).build();
+        }
     }
 }
